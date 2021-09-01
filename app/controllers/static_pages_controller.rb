9de5068c6f9cccc67e7b94411f226a84e7704706
Ubuntu <ubuntu@ip-172-31-37-157.us-east-2.compute.internal>
2021-09-01 03:09:37 +0000

    front end

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,13 @@
 class StaticPagesController < ApplicationController
+  
   def home
   end
 
   def help
   end
-
+  
   def about
   end
 
-  def contact
-  end
 
-  def singup
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+    # Returns the full title on a per-page basis.
+    def full_title(page_title = '')
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+            test
+                base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
+    
+    
+    
+    
 end
+    
+    
+
